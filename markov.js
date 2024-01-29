5e76a0ad59c0cdd36a727e2e278670f6d8895651
Neeti <neeti.i.bhatt@gmail.com>
2024-01-29 03:07:12 -0800

    final

--- markov.js
+++ markov.js
@@ -18,12 +18,38 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
-  }
+    let chain = new Map();
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+      if (chain.has(word)) chain.get(word).push(nextWord);
+      else chain.set(word, [nextWord]);
+    }
 
+    this.chain = chain;
+  }
+  static choice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    let keys = Array.from(this.chain.keys());
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    // produce markov chain until reaching termination word
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chain.get(key));
+    }
+
+    return out.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
